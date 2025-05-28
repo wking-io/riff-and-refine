fa1f8b6b1758b47155703a11fad4dc0aff58cab5
Will King <contact@wking.dev>
2025-05-28 10:42:51 -0500

    Updating to use picperf

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,7 +1,7 @@
 import { TextScramble, TextScrambleProvider } from '@/components/ui/text-scramble';
 import { Head } from '@inertiajs/react';
 
-const appUrl = 'https://media.riffandrefine.fm';
+const appUrl = 'https://picperf.io/https://media.riffandrefine.fm';
 
 export default function Welcome() {
     return (
@@ -11,13 +11,13 @@ export default function Welcome() {
                 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
             </Head>
             <div className="bg flex h-dvh flex-col items-center justify-center">
-                <div>
+                <div className="w-9/10 md:w-[500px]">
                     <picture>
                         <source srcSet={`${appUrl}/cover.webp 1x, ${appUrl}/cover@2x.webp 2x`} type="image/webp" />
                         <img
                             src={`${appUrl}/cover.webp`}
                             alt="Cover"
-                            className="ring-brand-blue/60 shadow-brand-blue/30 shadow-lg ring-1 transition duration-300 hover:-translate-y-0.5 hover:shadow-xl"
+                            className="ring-brand-blue/60 shadow-brand-blue/30 w-full shadow-lg ring-1 transition duration-300 hover:-translate-y-0.5 hover:shadow-xl"
                         />
                     </picture>
                     <div className="mt-3 flex w-full justify-between font-mono text-2xl opacity-90 mix-blend-screen">
